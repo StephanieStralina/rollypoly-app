1167d17db8db1ed3542d95b222c5cf7c1e398ece
StephanieStralina <StephanieDRichter@gmail.com>
2025-01-27 15:16:43 -0600

    Add handleChange to DiceForm

--- frontend/src/components/DiceForm/DiceForm.jsx
+++ frontend/src/components/DiceForm/DiceForm.jsx
@@ -9,12 +9,16 @@ export default function DiceForm() {
         modifier: 0,
     });
 
+    function handleChange(evt) {
+        setRollForm({ ...rollForm, [evt.target.name]: evt.target.value });
+    }
+
     return (
         <>
         <form>
-            <input type="number" name="numDice" value={rollForm.numDice} required/>
-            <input type="number" name="diceSides" value={rollForm.diceSides} required />
-            <input type="number" name="modifier" value={rollForm.modifier} required />
+            <input type="number" name="numDice" value={rollForm.numDice} onChange={handleChange} required/>
+            <input type="number" name="diceSides" value={rollForm.diceSides} onChange={handleChange} required />
+            <input type="number" name="modifier" value={rollForm.modifier} onChange={handleChange} required />
         </form>
         </>
     )
